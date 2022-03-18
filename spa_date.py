ffc5420f99ad7c4b94c893fd88755304742143d2
max <m.david@strong9.com>
2022-03-18 12:41:21 -0400

    pre implement of spa creation date

--- spa_date.py
+++ spa_date.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import datetime
 
 class SpaDate(object):
@@ -7,14 +6,24 @@ class SpaDate(object):
         self.cs_frame = cs_frame
     
     def add_spa_date_column(self):
-        self.cs_frame['spa_created_on'] = self.cs_frame.apply(lambda row: self.convert_serial_number(row),axis=1)
-        
+        temp_frame = self.cs_frame
+        temp_frame['spa_created_on'] = temp_frame.apply(lambda row: self.convert_serial_number(row),axis=1)
+        return temp_frame
 
     def convert_serial_number(self, row):
-        raw = row['Serial Number'][:6]
-        year = '20' + raw[-2:]
-        month = raw[:2]
-        day = raw[2:4]
-        date_str = datetime.date(int(year),int(month),int(day))
+        raw = str(row['Serial Number'])[:6]
+        try:
+            if raw.isnumeric():
+                year = '20' + raw[-2:]
+                month = raw[:2]
+                if int(month) not in range(1,13):
+                    return f'invalid format: {raw} *** month: {month}'
+                day = raw[2:4]
+                date_str = datetime.date(int(year),int(month),int(day))
+                
+                return date_str
+        except:
+            a = f"invalid format {raw}"
+            print(a)
+            return a
         
-        return date_str
